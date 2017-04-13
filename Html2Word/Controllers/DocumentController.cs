2fe9a35e2b7e673787b5288c571f2796f4e7f62f
elangelaar <ellenlangelaar@gmail.com>
2017-04-13 13:53:45 +0200

    Cleaned up DocumentController

--- Html2Word/Controllers/DocumentController.cs
+++ Html2Word/Controllers/DocumentController.cs
@@ -1,13 +1,8 @@
 ﻿using Html2Word.Models;
-using Novacode;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Web;
 using System.Web.Http;
 
 namespace Html2Word.Controllers
