d327edc93a16fcc0f2e772ec54dcd1fe826de852
elangelaar <ellenlangelaar@gmail.com>
2017-04-18 15:23:32 +0200

    Added a home controller with some basic info on use

--- Html2Word/App_Start/WebApiConfig.cs
+++ Html2Word/App_Start/WebApiConfig.cs
@@ -15,6 +15,18 @@ namespace Html2Word
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
+                name: "Index",
+                routeTemplate: "",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "Status",
+                routeTemplate: "api",
+                defaults: new { controller = "Home", action = "Status" }
+            );
+
+            config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
---
+++ Html2Word/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web.Http;
+
+namespace Html2Word.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Index()
+        {
+            var response = new HttpResponseMessage
+            {
+                Content = new StringContent(
+                    "<html><body><h1>Document Zip Generate</h1>Use in Angular example: <br> <table border='1'><tr><td>" +
+                    "$http.post('" + Request.RequestUri.GetLeftPart(UriPartial.Authority) + "/api/document/generate',{ Html : html });" +
+                    "</td></tr></table></body></html>"
+                    )
+            };
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return response;
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Status()
+        {
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(
+                    "{ Status: 'OK' }",
+                    Encoding.UTF8,
+                    "application/json"
+                )
+            };
+        }
+
+
+    }
+}
